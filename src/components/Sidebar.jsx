6bb956ecbffe63ea6ebe326052bad8f781b9b2e4
George Bassey <georgebassey1602@gmail.com>
2025-08-30 15:10:11 +0100

    sidebar changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,7 +18,6 @@ const Sidebar = () => {
   const [isActive, setIsActive] = useState(3);
   const [isOpen, setIsOpen] = useState(true);
   return (
-
     <aside
       className={`border-r-gray-700 dark:border-r-gray-100 p-6 dark:bg-gray-800 transition-all ${
         isOpen ? "w-1/6" : "w-[5%]"
@@ -34,36 +33,49 @@ const Sidebar = () => {
       )}
 
       <nav className="flex text-gray-800 justify-end p-2 dark:text-gray-100">
-        
         <button onClick={() => setIsOpen(!isOpen)} className="cursor-pointer">
           {isOpen ? <ChevronLeft size={20} /> : <ChevronRight size={20} />}
         </button>
       </nav>
 
       {/* active button on blue background, menuList hidden on small screen but visible on big screen */}
-      <ul className="mt-4">
-        {menuList.map((menu) => (
-          <li
-            key={menu.id}
-            className={`flex items-center gap-2 cursor-pointer p-2 justify-center
-        ${
-          isActive === menu.id
-            ? `text-white ${
-                isOpen ? "md:bg-blue-700 md:rounded-full" : ""
-              } transition-colors duration-300`
-            : "text-gray-800 dark:text-gray-200"
-        }
-        ${isOpen ? "gap-2" : "rounded-md"}
-      `}
-          >
-            <span className="w-8 h-8 flex items-center justify-center">
-              {menu.icon}
-            </span>
-            {isOpen && (
-              <span className="hidden w-5 h-5 md:block">{menu.name}</span>
-            )}
-          </li>
-        ))}
+      <ul className="mt-4 flex flex-col">
+        {menuList.map((menu) => {
+          const isActiveItem = isActive === menu.id;
+
+          return (
+            <li
+              key={menu.id}
+              className={`
+          flex items-center p-2 cursor-pointer
+          rounded-md
+          transition-colors duration-300
+          ${
+            isActiveItem
+              ? "text-gray-800 dark:text-gray-200 md:bg-blue-700 md:text-white"
+              : "text-gray-800 dark:text-gray-200"
+          }
+          ${
+            isOpen
+              ? "gap-2 justify-start w-full bg-transparent"
+              : "justify-center"
+          }
+        `}
+            >
+              {/* Icon */}
+              <span
+                className={`
+            w-8 h-8 flex items-center justify-center fill-current
+          `}
+              >
+                {menu.icon}
+              </span>
+
+              {/* Text (only visible when sidebar is open) */}
+              {isOpen && <span className="hidden md:block">{menu.name}</span>}
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
